4c35208aae3177c235b8484910baba237d83be26
Daniel adjei <dadjei@quantumgroupgh.com>
2023-05-23 15:04:43 +0000

    add paddings to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        <h1>about page</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sed quis aspernatur assumenda, explicabo cumque nobis adipisci ducimus fugiat repudiandae quos in dolore quibusdam. Repellendus unde placeat voluptatem ea temporibus.</p>
+    <div class="p-4">
+        <div>
+            <h1 class="text-4xl mb-2">About page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sed quis aspernatur assumenda, explicabo cumque nobis adipisci ducimus fugiat repudiandae quos in dolore quibusdam. Repellendus unde placeat voluptatem ea temporibus.</p>
+        </div>
     </div>
 </template>
 
