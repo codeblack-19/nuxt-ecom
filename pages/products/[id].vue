08f8d864b9592b7ff7bb1d76b91ec0697b544c93
Daniel adjei <dadjei@quantumgroupgh.com>
2023-05-23 15:03:53 +0000

    change head data and create error on 404

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,9 @@
 <template>
     <div>
+        <Head>
+            <Title>Nuxt Ecom | {{ product?.title }}</Title>
+            <Meta name="description" :content="product?.description" />
+        </Head>
         <ProductDetails :product="product" />
     </div>
 </template>
@@ -11,11 +15,19 @@ const { id } = useRoute().params
 const uri = 'https://fakestoreapi.com/products/' + id
 
 definePageMeta({
-    layouts: 'products'
+    layout: 'products'
 })
 
 // fetch the product
 const { data: product } = await useFetch<ProductType>(uri)
+
+if(!product.value){
+    throw createError({
+        statusCode: 404,
+        statusMessage: 'Product Not found',
+        fatal: true
+    })
+}
 
 </script>
 
